7866940445c51088a9b35f3c65a5d2ad0d662e80
JasonL742 <jasonliu741@gmail.com>
2025-03-07 22:33:17 -0500

    fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SignIn from './Components/Navbar/signin.jsx';
 import Home from './Home.jsx'
-import folderpage from './folderpage.jsx';
+import Folderpage from './folderpage.jsx';
 const App = () => {
   return (
     <Routes>
        <Route path="/Home" element={<Home />} />
       <Route path="/signin" element={<SignIn />} />
-      <Route path="/folderpage" element={<folderpage />} />
+      <Route path="/folderpage" element={<Folderpage />} />
     </Routes>
   );
 };
