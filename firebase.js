7e13028567c69e7f4c462aef21da2234340f0af3
JasonL742 <jasonliu741@gmail.com>
2025-03-09 09:16:29 -0400

    hid more keys

--- firebase.js
+++ firebase.js
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRIF4544FjnRrTMxEaYpk9KsUga15dqgg",
-  authDomain: "tcgportfolio-94e6f.firebaseapp.com",
-  projectId: "tcgportfolio-94e6f",
-  storageBucket: "tcgportfolio-94e6f.firebasestorage.app",
-  messagingSenderId: "153714450173",
-  appId: "1:153714450173:web:45ae3732b3df2ca7186894",
-  measurementId: "G-B1FCVWP95W"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId
 };
 
 // Initialize Firebase
