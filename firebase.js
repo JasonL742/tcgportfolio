f3d9acf01e47d358aad95ef8467fd9d2f9e32721
JasonL742 <jasonliu741@gmail.com>
2025-03-10 15:42:42 -0400

    q

--- firebase.js
+++ firebase.js
@@ -11,7 +11,7 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
+  projectId: "tcgportfolio-94e6f",
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId,
